44ed2aa498d6ae955c730825cd2b088bcc7ed583
JoannaPokora <pokorajo@gmail.com>
2026-04-28 14:36:15 +0200

    added imputation after prediction

--- modules/predict.py
+++ modules/predict.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from .utils import build_rating_matrix
 
-def predict_nmf(test_file, model_data):
+def predict(test_file, model_data):
     """
     Reads a test CSV with columns: userId, movieId.
     Uses the stored Z_approx, user_map, movie_map to produce predictions.
@@ -16,55 +16,27 @@ def predict_nmf(test_file, model_data):
 
     Z_approx, user_map, movie_map = model_data
 
-    predictions = []
-    for row in df.itertuples():
+    def predict_rating(row):
         u = row.userId
         m = row.movieId
         if u in user_map and m in movie_map:
             i = user_map[u]
             j = movie_map[m]
-            rating = Z_approx[i, j]
+            return Z_approx[i, j]
         else:
-            rating = np.nan
+            return np.nan
+        
+    df["rating"] = df.apply(predict_rating, axis = 1)
 
-        predictions.append({
-            "userId": u,
-            "movieId": m,
-            "rating": rating
-        })
-
-    return predictions
-
-
-def predict_svd1(test_file, model_data):
-    """
-    Reads a test CSV with columns: userId, movieId.
-    Uses the stored Z_approx, user_map, movie_map to produce predictions.
-    Missing userId/movieId combos produce a default rating (e.g., 0 or average).
-
-    Returns a list of dicts with keys: 'userId', 'movieId', 'rating'.
-    """
-    df = pd.read_csv(test_file)
-
-    Z_approx = model_data["Z_approx"]
-    user_map = model_data["user_map"]
-    movie_map = model_data["movie_map"]
+    # imputation
+    df["rating"] = df["rating"].fillna(df.groupby("movieId")["rating"].transform("mean"))
 
     predictions = []
     for row in df.itertuples():
-        u = row.userId
-        m = row.movieId
-        if u in user_map and m in movie_map:
-            i = user_map[u]
-            j = movie_map[m]
-            rating = Z_approx[i, j]
-        else:
-            rating = 0
-        rating_rounded = round(rating, 1)
-
         predictions.append({
-            "userId": u,
-            "movieId": m,
-            "rating": rating_rounded
+            "userId": row.userId,
+            "movieId": row.movieId,
+            "rating": row.rating
         })
+
     return predictions
